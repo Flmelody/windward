498fabb7d25c4a32621f003fd4bf4c5fa11f2786
esotericman <justmysteriousman@163.com>
2023-08-19 16:40:19 +0800

    Set charset UTF-8 for response since not all browsers use UTF-8 default

--- src/main/java/org/flmelody/core/MediaType.java
+++ src/main/java/org/flmelody/core/MediaType.java
@@ -18,9 +18,9 @@ package org.flmelody.core;
  * @author esotericman
  */
 public class MediaType {
-  public static final String APPLICATION_JSON_VALUE = "application/json";
+  public static final String APPLICATION_JSON_VALUE = "application/json;charset=UTF-8";
   public static final String MULTIPART_FORM_DATA_VALUE = "multipart/form-data";
-  public static final String TEXT_PLAIN_VALUE = "text/plain";
+  public static final String TEXT_PLAIN_VALUE = "text/plain;charset=UTF-8";
 
   private MediaType() {}
 }
