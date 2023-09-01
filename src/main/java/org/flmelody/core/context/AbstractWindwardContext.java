00ee4d247b088e4f7a25eac239819dfd30a445e0
esotericman <justmysteriousman@163.com>
2023-09-01 08:07:01 +0800

    Build view url

--- src/main/java/org/flmelody/core/context/AbstractWindwardContext.java
+++ src/main/java/org/flmelody/core/context/AbstractWindwardContext.java
@@ -30,6 +30,7 @@ import org.flmelody.core.WindwardResponse;
 import org.flmelody.core.exception.WindwardException;
 import org.flmelody.core.plugin.json.JsonPlugin;
 import org.flmelody.core.plugin.view.ViewPlugin;
+import org.flmelody.util.UrlUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -238,7 +239,7 @@ public class AbstractWindwardContext implements WindwardContext {
       try {
         String renderedView =
             viewPlugin.render(
-                ViewPlugin.templateLocationPrefix + "/" + viewUrl,
+                UrlUtil.buildUrl(ViewPlugin.templateLocationPrefix, viewUrl),
                 Windward.plugin(JsonPlugin.class).toObject(model, HashMap.class));
         windwardResponse.write(
             HttpStatus.OK.value(), MediaType.TEXT_HTML_VALUE, Collections.emptyMap(), renderedView);
